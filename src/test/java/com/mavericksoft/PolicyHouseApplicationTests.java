947b34d6fa0132ed02fc93c74130742df9f8a70f
kaushiku <kaushik.udavant@kpit.com>
2020-08-19 23:59:04 +0530

    Whole application

---
+++ src/test/java/com/mavericksoft/PolicyHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
